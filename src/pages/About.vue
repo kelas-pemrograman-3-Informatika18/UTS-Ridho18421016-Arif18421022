0480842fabdaf6efd3718bfb8fabe243e9fa14cb
Ryukikun <ridhodzaki03@gmail.com>
2020-04-30 09:44:19 +0700

    first commit

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -31,6 +31,37 @@
         </q-card-section>
       </q-card-section>
     </q-card>
+    <q-card flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Muhammad Arif N </q-item-label>
+          <q-item-label caption>
+            Mahasiswa UBL FIK , Angkatan 18
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-card-section horizontal>
+        <q-card-section>
+          {{ data1 }}
+        </q-card-section>
+
+        <q-separator vertical />
+
+        <q-card-section class="col-4">
+          <q-item-label>Gmail : arief03@gmail.com </q-item-label>
+          <q-item-label>WA : 081218180857 </q-item-label>
+        </q-card-section>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -38,7 +69,8 @@
 export default {
   data () {
     return {
-      data: 'Dia adalah seorang mahasiswa yang senang sekali yang berkaitan tentang programmer , dalam pembuatan aplikasi ini , dia berada di semua bidang'
+      data: 'Dia adalah seorang mahasiswa yang senang sekali yang berkaitan tentang programmer , dalam pembuatan aplikasi ini , dia berada di semua bidang',
+      data1: 'Dia adalah seorang mahasiswa UBl , dalam pembuatan aplikasi ini , dia berada di bagian pendesainya'
     }
   }
 }
